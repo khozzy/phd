a9b17d3fbba21b58ad359d455e027c6fdc5d9d46
Norbert Kozlowski <norbert.kozlowski@arcsoftware.it>
2022-01-22 09:45:56 +0100

    diminishing reward in woods1

--- src/observation_wrappers.py
+++ src/observation_wrappers.py
@@ -12,6 +12,24 @@ class FSWObservationWrapper(gym.ObservationWrapper):
         return observation,
 
 
+class WoodsBinaryAdapter(gym.ObservationWrapper):
+    def observation(self, observation):
+        result = []
+        for el in observation:
+            if el == 'F':
+                result.extend(['1', '1', '0'])
+            if el == 'G':
+                result.extend(['1', '1', '1'])
+            if el == 'O':
+                result.extend(['0', '1', '0'])
+            if el == 'Q':
+                result.extend(['0', '1', '1'])
+            if el == '.':
+                result.extend(['0', '0', '0'])
+
+        return result
+
+
 class BinnedObservationWrapper(gym.ObservationWrapper):
     """
     Divide observation using environment's observation space into `num_bins` equal bins.
